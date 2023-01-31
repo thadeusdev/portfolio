a54f8e09c71b075bfe656e1e3a888aec68849866
thadeus <thadeusnyariki94@gmail.com>
2023-01-31 16:02:40 +0300

    done with products & productList

---
+++ src/data.js
@@ -0,0 +1,34 @@
+import Dashboard from './img/Dashboard.png'
+export const products = [
+    {
+      id: 1,
+      img: "https://raw.githubusercontent.com/thadeusdev/hospital-management-system/main/client/src/img/Dashboard.png",
+      link: "https://github.com/thadeusdev",
+    },
+    {
+      id: 2,
+      img: "https://raw.githubusercontent.com/thadeusdev/hospital-management-system/main/client/src/img/Patients.png",
+      link: "https://github.com/thadeusdev",
+    },
+    {
+      id: 3,
+      img: "https://raw.githubusercontent.com/thadeusdev/hospital-management-system/main/client/src/img/data-diagnose.png",
+      link: "https://github.com/thadeusdev",
+    },
+    {
+      id: 4,
+      img: "https://raw.githubusercontent.com/thadeusdev/hospital-management-system/main/client/src/img/data-diagnose.png",
+      link: "https://github.com/thadeusdev",
+    },
+    {
+      id: 5,
+      img: "https://raw.githubusercontent.com/thadeusdev/hospital-management-system/main/client/src/img/data-diagnose.png",
+      link: "https://github.com/thadeusdev",
+    },
+    {
+      id: 6,
+      img: "https://raw.githubusercontent.com/thadeusdev/hospital-management-system/main/client/src/img/data-diagnose.png",
+      link: "https://github.com/thadeusdev",
+    },
+  ];
+  
