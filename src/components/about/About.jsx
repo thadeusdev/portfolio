fde36dc43f6e52cbf4ec7c6a89575321193c8e1a
thadeus <thadeusnyariki94@gmail.com>
2023-01-31 13:39:40 +0300

    completed about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import './about.css'
+import Code from '../../img/code.jpg'
+import Cert from '../../img/award.png'
 
 const About = () => {
   return (
-    <div className='a'>About</div>
+    <div className='a'>
+        <div className="a-left">
+            <div className="a-card bg"></div>
+            <div className="a-card">
+                <img src={Code} alt="" className="a-img" />
+            </div>
+        </div>
+        <div className="a-right">
+            <h1 className="a-title">About Me</h1>
+            <p className="a-sub">
+            Full-Stack software Engineer with experience in building and deploying web applications.
+            </p>
+            <p className="a-desc">
+            Skilled in developing and maintaining the front-end and back-end components of web applications using technologies such as JavaScript, React, Ruby and Ruby On Rails. 
+            </p>
+            <p className="a-desc">
+            Strong knowledge of database design and management using SQL and NoSQL databases. 
+            </p>
+            <p className="a-desc">
+            Experienced in Agile development methodologies and familiar with DevOps tools and practices. 
+            </p>
+            <p className="a-desc">
+            Strong problem-solving and analytical skills with a passion for high-quality, user-friendly software.
+            </p>
+            <div className="a-award">
+                <img src={Cert} alt="" className="a-award-img" />
+                <div className="a-award-texts">
+                    <h4 className="a-award-title">
+                    Project Management Foundations (2019)
+                    </h4>
+                    <p className="a-award-desc">
+                    LinkedIn Learning Certificate of Completion.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
