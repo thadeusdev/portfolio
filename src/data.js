483185a5dc92bc140239ad8816e51d905851df1d
thadeus <thadeusnyariki94@gmail.com>
2023-02-01 04:30:13 +0300

    added img to data

--- src/data.js
+++ src/data.js
@@ -2,32 +2,17 @@ export const products = [
     {
       id: 1,
       img: "https://raw.githubusercontent.com/thadeusdev/hospital-management-system/main/client/src/img/Dashboard.png",
-      link: "https://github.com/thadeusdev",
+      link: "https://raw.githubusercontent.com/thadeusdev/hospital-management-system/main/client/src/img/Dashboard.png",
     },
     {
       id: 2,
-      img: "https://raw.githubusercontent.com/thadeusdev/hospital-management-system/main/client/src/img/Patients.png",
-      link: "https://github.com/thadeusdev",
+      img: "https://raw.githubusercontent.com/thadeusdev/portfolio/main/src/img/car-review1%20(1).png",
+      link: "https://raw.githubusercontent.com/thadeusdev/portfolio/main/src/img/car-review1%20(1).png",
     },
     {
       id: 3,
       img: "https://raw.githubusercontent.com/thadeusdev/hospital-management-system/main/client/src/img/data-diagnose.png",
-      link: "https://github.com/thadeusdev",
-    },
-    {
-      id: 4,
-      img: "https://raw.githubusercontent.com/thadeusdev/hospital-management-system/main/client/src/img/data-diagnose.png",
-      link: "https://github.com/thadeusdev",
-    },
-    {
-      id: 5,
-      img: "https://raw.githubusercontent.com/thadeusdev/hospital-management-system/main/client/src/img/data-diagnose.png",
-      link: "https://github.com/thadeusdev",
-    },
-    {
-      id: 6,
-      img: "https://raw.githubusercontent.com/thadeusdev/hospital-management-system/main/client/src/img/data-diagnose.png",
-      link: "https://github.com/thadeusdev",
+      link: "https://raw.githubusercontent.com/thadeusdev/hospital-management-system/main/client/src/img/data-diagnose.png",
     },
   ];
   
