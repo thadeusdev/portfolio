bc48b64b7fd5938a8d6c5be4936f730bdb3e13a9
thadeus <thadeusnyariki94@gmail.com>
2023-03-06 15:27:41 +0300

    added more projects

--- src/data.js
+++ src/data.js
@@ -2,15 +2,40 @@ export const products = [
     {
       id: 1,
       img: "https://raw.githubusercontent.com/thadeusdev/hospital-management-system/main/client/src/img/Dashboard.png",
-      link: "https://raw.githubusercontent.com/thadeusdev/hospital-management-system/main/client/src/img/Dashboard.png",
+      link: "https://tranquil-falls-82915.herokuapp.com/",
     },
     {
       id: 2,
-      img: "https://raw.githubusercontent.com/thadeusdev/portfolio/main/src/img/car-review1%20(1).png",
-      link: "https://raw.githubusercontent.com/thadeusdev/portfolio/main/src/img/car-review1%20(1).png",
+      img: "https://raw.githubusercontent.com/thadeusdev/birthday-reminder/main/src/img/birthday-reminder.png",
+      link: "https://birthday-reminder-thadeus.netlify.app/",
     },
     {
       id: 3,
+      img: "https://raw.githubusercontent.com/thadeusdev/accordion/main/src/img/accordion.png",
+      link: "https://accordion-thadeus.netlify.app/",
+    },
+    {
+      id: 4,
+      img: "https://raw.githubusercontent.com/thadeusdev/menu/main/src/img/menu.png",
+      link: "https://menu-thadeus.netlify.app/",
+    },
+    {
+      id: 5,
+      img: "https://raw.githubusercontent.com/thadeusdev/tabs/main/src/img/tabs.png",
+      link: "https://tabs-thadeus.netlify.app/",
+    },
+    {
+      id: 6,
+      img: "https://raw.githubusercontent.com/thadeusdev/reviews/main/src/img/review.png",
+      link: "https://review-thadeus.netlify.app/",
+    },
+    {
+      id: 7,
+      img: "https://raw.githubusercontent.com/thadeusdev/tours/main/src/img/tours.png",
+      link: "https://tours-thadeus.netlify.app/",
+    },
+    {
+      id: 8,
       img: "https://raw.githubusercontent.com/thadeusdev/hospital-management-system/main/client/src/img/data-diagnose.png",
       link: "https://raw.githubusercontent.com/thadeusdev/hospital-management-system/main/client/src/img/data-diagnose.png",
     },
